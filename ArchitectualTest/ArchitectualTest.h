b98a1010dbbc827addfe57be7b898f19512c588e
EugeneHoldem <eugeneholdem@gmail.com>
2018-12-23 23:49:58 +0200

    Initial Commit

---
+++ ArchitectualTest/ArchitectualTest.h
@@ -0,0 +1,19 @@
+//
+//  ArchitectualTest.h
+//  ArchitectualTest
+//
+//  Created by Eugene Henyk on 12/23/18.
+//  Copyright © 2018 Babysits B.V. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ArchitectualTest.
+FOUNDATION_EXPORT double ArchitectualTestVersionNumber;
+
+//! Project version string for ArchitectualTest.
+FOUNDATION_EXPORT const unsigned char ArchitectualTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArchitectualTest/PublicHeader.h>
+
+
---
+++ ArchitectualTest/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  ArchitectualTest
+//
+//  Created by Eugene Henyk on 12/23/18.
+//  Copyright © 2018 Babysits B.V. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
